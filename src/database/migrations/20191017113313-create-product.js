deb5c7e3a224be78d6cda38f82d252c4e3c2a1bd
Junior Alves <jjalves96@gmail.com>
2019-10-18 05:37:58 -0300

    Feature Migration Create Product

---
+++ src/database/migrations/20191017113313-create-product.js
@@ -0,0 +1,51 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      unit: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('products');
+  },
+};
