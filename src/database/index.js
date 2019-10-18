60febdfe78adefc64fd803c7f36a129fa820c5f8
Junior Alves <jjalves96@gmail.com>
2019-10-18 05:38:27 -0300

    Feature Add Produtc Model

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+import Product from '../app/models/Product';
 
-const models = [User];
+const models = [User, Product];
 
 class Database {
   constructor() {
